f8d865fce7dae4b709a982c0a06e8475085f2ead
mebelh <memetbelualov@bk.ru>
2020-10-21 00:10:54 +0300

    Fix redux

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import {Provider} from 'react-redux'
 
 const store = createStore(rootReducer, compose(
    applyMiddleware(thunk),
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 ReactDOM.render(
